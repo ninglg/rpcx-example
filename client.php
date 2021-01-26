2234316a18ba77ac5ebb5630a0e6f72d0ec2bc68
ninglg <ninglg@gmail.com>
2021-01-26 19:56:28 +0800

    init

---
+++ client.php
@@ -0,0 +1,20 @@
+<?php
+$url = 'http://localhost:9981/';
+$data = '{"A":20, "B":30}';
+// use key 'http' even if you send the request to https://...
+$options = array(
+    'http' => array(
+        'header'  => "Content-type: application/rpcx\r\n" .
+            // "X-RPCX-MessageID: 12345678\r\n" .
+            // "X-RPCX-MesssageType: 0\r\n" .
+            "X-RPCX-SerializeType: 1\r\n" .
+            "X-RPCX-ServicePath: Arith\r\n" .
+            "X-RPCX-ServiceMethod: Mul\r\n",
+        'method'  => 'POST',
+        'content' => $data
+    )
+);
+$context  = stream_context_create($options);
+$result = file_get_contents($url, false, $context);
+if ($result === FALSE) { /* Handle error */ }
+var_dump($result);
